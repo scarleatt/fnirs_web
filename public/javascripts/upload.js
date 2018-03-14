9df4ce8f790fbdf04855408b328a697b137bd508
resccu <fawnliu2333@gmail.com>
2018-03-14 22:33:39 +0800

    add py

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -24,7 +24,7 @@ $('#upload-input').on('change', function(){
             success: function(res){
                 console.log('upload successful!\n');
                 console.log(res);
-                alert('跳跃时间为 ' + res*100+ 'ms');
+                alert('跳跃时间为 ' + res+ 'ms');
             },
             xhr: function() {
                 var xhr = new XMLHttpRequest();
